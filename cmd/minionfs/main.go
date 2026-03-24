0f187ffca08f848a6d6a84c0ce755be8457a9125
stringcheeseguy <149595245+Prana-vvb@users.noreply.github.com>
2026-03-24 13:17:37 +0530

    feat: Basic mount operation

---
+++ cmd/minionfs/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
+	minionfs "github.com/Prana-vvb/minionfs/internal/fs"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Invalid usage. Use go run main.go <mntpoint>")
+		return
+	}
+
+	mount_point := os.Args[1]
+
+	//c is a fuse connection to dev/fuse
+	c, err := fuse.Mount(mount_point)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer c.Close() //delay execution of Close
+
+	err = fs.Serve(c, minionfs.FS{}) //starts listening for FS reqs
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// <-c.Ready
+	// if err := c.MountError; err != nil {
+	// 	fmt.Println(err)
+	// }
+}
